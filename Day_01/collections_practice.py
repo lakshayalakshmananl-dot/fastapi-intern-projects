3633e30fb4a99af0b06c1325e8feb876a0306fe7
Lakshaya Lakshmanan <lakshayalakshmanan.l@gmail.com>
2026-05-27 12:59:36 +0530

    Practiced lists tuples dicts and sets

---
+++ Day_01/collections_practice.py
@@ -0,0 +1,37 @@
+# LIST
+fruits = ["apple", "banana", "mango"]
+
+print("List:")
+print(fruits)
+
+fruits.append("orange")
+
+print("After adding orange:")
+print(fruits)
+
+
+# TUPLE
+colors = ("red", "blue", "green")
+
+print("\nTuple:")
+print(colors)
+
+
+# DICTIONARY
+student = {
+    "name": "Lakshaya",
+    "age": 19,
+    "course": "Python"
+}
+
+print("\nDictionary:")
+print(student)
+
+print("Student Name:", student["name"])
+
+
+# SET
+numbers = {1, 2, 3, 3, 4, 4, 5}
+
+print("\nSet:")
+print(numbers)
