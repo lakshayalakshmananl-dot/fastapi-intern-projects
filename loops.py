4ab19e68d4f52a4cb2c07f2f35262ea9f5ce9dc2
Lakshaya Lakshmanan <lakshayalakshmanan.l@gmail.com>
2026-05-27 12:43:39 +0530

    chore: organized Day 2 tasks into dedicated folder

---
+++ loops.py
@@ -0,0 +1,18 @@
+# if else loop practice
+list=int(input("Enter a number :"))
+for i in list:
+    if i%2==0:
+        print("EVEN NUMBER")
+    else:
+        print("ODD NUMBER")
+# for  loop practice
+input=[2,4,6,8,10,12,14,16,18,20]
+for i in input:
+    print(i)
+#while loop practice
+count=5
+while count > 0:
+    print(count)
+    count=count - 1
+print("LAUNCH")
+
