7bd934fad56e99b5cef6b23f18169160a8ed35af
Lakshaya Lakshmanan <lakshayalakshmanan.l@gmail.com>
2026-05-27 14:56:03 +0530

    Add Day 03 projects

--- Day_03/loops.py
+++ Day_03/loops.py
@@ -1,18 +1,19 @@
 # if else loop practice
-list=int(input("Enter a number :"))
-for i in list:
-    if i%2==0:
-        print("EVEN NUMBER")
-    else:
-        print("ODD NUMBER")
-# for  loop practice
-input=[2,4,6,8,10,12,14,16,18,20]
-for i in input:
+number = int(input("Enter a number :"))
+
+if number % 2 == 0:
+    print("EVEN NUMBER")
+else:
+    print("ODD NUMBER")
+
+# for loop practice
+numbers_list = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+for i in numbers_list:
     print(i)
-#while loop practice
-count=5
+
+# while loop practice
+count = 5
 while count > 0:
-    print(count)
-    count=count - 1
-print("LAUNCH")
+    print(f"Countdown: {count}")
+    count -= 1
 
