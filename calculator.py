1c8018c0c36506e4bd492b0dcbcd224737a2e654
Lakshaya Lakshmanan <lakshayalakshmanan.l@gmail.com>
2026-05-27 11:13:15 +0530

    added calculator program

---
+++ calculator.py
@@ -0,0 +1,27 @@
+def add(a: float, b: float) -> float:
+    return a + b
+
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+
+def divide(a: float, b: float):
+
+    if b == 0:
+        return "Cannot divide by zero"
+
+    return a / b
+
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
